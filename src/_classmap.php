8a5bfdb2d6bb07891f08098417096fafdd1007f8
Thomas Weinert <weinert@dimensional.de>
2014-05-02 15:17:59 +0200

    # Initial Commit, separate module from cms/framework repository

---
+++ src/_classmap.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+  'actionbox_comments' => '/actbox_comments.php',
+  'actionbox_forum' => '/actbox_forum.php',
+  'actionbox_lastentries' => '/actbox_lastentries.php',
+  'admin_forum' => '/admin_forum.php',
+  'base_forum' => '/base_forum.php',
+  'box_forum' => '/box_forum.php',
+  'connector_forum' => '/connector_forum.php',
+  'content_forum' => '/content_forum.php',
+  'content_forum_lastentries' => '/content_forum_lastentries.php',
+  'edmodule_forum' => '/edmodule_forum.php',
+  'output_forum' => '/output_forum.php',
+);
---
+++ src/edmodule_forum.php
@@ -0,0 +1,116 @@
+<?php
+/**
+* Forum management.
+*
+* @copyright 2002-2011 by papaya Software GmbH - All rights reserved.
+* @link http://www.papaya-cms.com/
+* @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+*
+* You can redistribute and/or modify this script under the terms of the GNU General Public
+* License (GPL) version 2, provided that the copyright and license notes, including these
+* lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.
+*
+* @package Papaya-Modules
+* @subpackage Free-Forum
+* @version $Id: edmodule_forum.php 39508 2014-03-04 14:05:40Z weinert $
+*/
+
+/**
+* Forum management.
+*
+* @package Papaya-Modules
+* @subpackage Free-Forum
+*/
+class edmodule_forum extends base_module {
+
+  /**
+  * Permissions
+  * @var array $permissions
+  */
+  public $permissions = array(
+    1 => 'Manage',
+    2 => 'Edit categories',
+    3 => 'Edit forums',
+    4 => 'Edit entries'
+  );
+
+  /**
+  * Plugin option fields
+  * @var array $pluginOptionFields
+  */
+  public $pluginOptionFields = array(
+    'REJECT_ATTACK_STRINGS' => array(
+      'Reject Potential Attack Strings',
+      'isSomeText',
+      FALSE,
+      'input',
+      100,
+      'List the character combinations to reject, separated by blanks',
+      ''
+    ),
+    'ENTRY_MAX_LENGTH' => array(
+      'Maximum Entry Length',
+      'isNum',
+      TRUE,
+      'input',
+      5,
+      'Enter 0 for unlimited entries',
+      0
+    ),
+    'SURFER_LIMIT' => array(
+      'Surfer Limit',
+      'isNum',
+      TRUE,
+      'input',
+      5,
+      'Enter the shown surfers per page',
+      20
+    ),
+    'THREAD_LIMIT' => array(
+      'Thread Limit',
+      'isNum',
+      TRUE,
+      'input',
+      5,
+      'Enter the shown threads per page',
+      20
+    ),
+    'BOARD_LIMIT' => array(
+      'Board Limit',
+      'isNum',
+      TRUE,
+      'input',
+      5,
+      'Enter the shown boards per page',
+      20
+    ),
+    'THREAD_UPDATE_TIME_MODE' => array(
+      'Thread Update Get Time',
+      'isNum',
+      TRUE,
+      'translatedcombo',
+      array(
+        0 => 'By current time',
+        1 => 'By last entries\' time'
+      ),
+      NULL,
+      0
+    )
+  );
+
+  /**
+  * Execute module
+  */
+  public function execModule() {
+    if ($this->hasPerm(1, TRUE)) {
+      $forum = new admin_forum;
+      $forum->module = $this;
+      $forum->layout = $this->layout;
+      $forum->initialize();
+      $forum->execute();
+      $forum->getXML();
+    }
+  }
+}
